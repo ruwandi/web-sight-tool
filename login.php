c78a694576712aa118217c2cae2fe8c63c5f60a4
Ruwandi Madhunamali <ruwandi1102@gmail.com>
2018-05-12 12:29:03 +0530

    This is login page\

---
+++ login.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="Register.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Register</h2>
+	</div>	
+	<form method="post" action="register.php">	
+		<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="Username"></input>
+	</div> 
+
+	<div class="input-group">
+		<label>Password</label>
+		<input type="text" name="password"></input>
+	</div> 
+    </div> 
+
+	<div class="input-group">
+		
+		<butten type="submit" name="login" class="but">Login</butten>
+	</div> 
+	<p>Already a member? <a href="register.php.php">Sign in</a>
+	</p>
+
+	</form> 
+
+</body>
+</html>
